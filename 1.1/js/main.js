188f5cc700ab6595c1de2bd613233282e8fa4eb3
AnderRester <AnderRester@mail.ru>
2021-01-06 13:12:40 +0200

    1.1

---
+++ 1.1/js/main.js
@@ -0,0 +1,36 @@
+let num = prompt("Введите число", 0);
+
+switch (num) {
+    case "0":
+        alert("Ваше число - 0");
+        break;
+    case "1":
+        alert("Ваше число - 1");
+        break;
+    case "2":
+        alert("Ваше число - 2");
+        break;
+    case "3":
+        alert("Ваше число - 3");
+        break;
+    case "4":
+        alert("Ваше число - 4");
+        break;
+    case "5":
+        alert("Ваше число - 5");
+        break;
+    case "6":
+        alert("Ваше число - 6");
+        break;
+    case "7":
+        alert("Ваше число - 7");
+        break;
+    case "8":
+        alert("Ваше число - 8");
+        break;
+    case "9":
+        alert("Ваше число - 9");
+        break;
+}
+
+//alert(`Ваше число ${num}`);
